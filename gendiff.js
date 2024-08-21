0f5c0f0f0f328a53cbd2eba013ea840ae781a217
Artem <labauriatm@gmail.com>
2024-08-21 21:34:06 +0300

    Added two new files in root directory and parse them.

--- gendiff.js
+++ gendiff.js
@@ -1,26 +1,27 @@
 #!/usr/bin/env node
 
 const { Command } = require('commander');
+const fs = require('fs');
+const path = require('path');
 const program = new Command();
+
+const parseFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+  return JSON.parse(fileContent);
+};
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'output usage information');
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    const data1 = parseFile(filepath1);
+    const data2 = parseFile(filepath2);
 
-program.on('--help', () => {
-  console.log();
-  console.log('Usage: gendiff [options] <filepath1> <filepath2>');
-  console.log();
-  console.log('Compares two configuration files and shows a difference.');
-  console.log();
-  console.log('Options:');
-  console.log('  -V, --version        output the version number');
-  console.log('  -f, --format [type]  output format');
-  console.log('  -h, --help           output usage information');
-  console.log();
-});
+    console.log('File 1 data:', data1);
+    console.log('File 2 data:', data2);
+  });
 
 program.parse(process.argv);
