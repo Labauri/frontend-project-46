291b7154bea10b13e809289d858e4caf906ec3a3
Artem <labauriatm@gmail.com>
2024-08-22 18:59:06 +0300

    Added new library: lodash. Also modified file gendiff.

--- gendiff.js
+++ gendiff.js
@@ -3,6 +3,7 @@
 const { Command } = require('commander');
 const fs = require('fs');
 const path = require('path');
+const _ = require('lodash');
 const program = new Command();
 
 const parseFile = (filepath) => {
@@ -10,6 +11,29 @@ const parseFile = (filepath) => {
   const fileContent = fs.readFileSync(absolutePath, 'utf-8');
   return JSON.parse(fileContent);
 };
+
+
+const compareFiles = (data1, data2) => {
+  const keys = _.union(_.keys(data1), _.keys(data2));
+
+  return keys.map((key) => {
+    if (!_.has(data2, key)) {
+      return `- ${key}: ${data1[key]}`;
+    }
+
+    if (!_.has(data1, key)) {
+      return `+ ${key}: ${data2[key]}`;
+    }
+
+    if (!_.isEqual(data1[key], data2[key])) {
+      return [
+          `- ${key}: ${data1[key]}`,
+          `+ ${key}: ${data2[key]}`
+      ].join('\n')
+    }
+    return ` ${key}: ${data1[key]}`;
+  }).join('\n');
+}
 
 program
   .name('gendiff')
@@ -20,8 +44,8 @@ program
     const data1 = parseFile(filepath1);
     const data2 = parseFile(filepath2);
 
-    console.log('File 1 data:', data1);
-    console.log('File 2 data:', data2);
+    const diff = compareFiles(data1, data2);
+    console.log(diff);
   });
 
 program.parse(process.argv);
