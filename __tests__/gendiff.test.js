413923c416cbf81eea4f8701771ffefeac114935
Artem <labauriatm@gmail.com>
2024-11-05 20:28:08 +0300

    Renamed compare.test files.

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,7 +12,7 @@ test('test1 genDiff .json default=stylish formatter', () => {
   const file1 = getFixture('file1.json');
   const file2 = getFixture('file2.json');
   const response = gendiff(file1, file2);
-  const fileContent = readFixtureFile('files_12_stylish_result.txt');
+  const fileContent = readFixtureFile('stylish_result.txt');
   expect(response).toEqual(fileContent);
 });
 
@@ -43,6 +43,6 @@ test('test4 genDiff JSON formatter', () => {
 test('test5 format error', () => {
   const file1 = getFixture('file1.yml');
   const file2 = getFixture('file2.yml');
-  const response = gendiff(file1, file2, '123');
+  const response = gendiff(file1, file2, 'qwe123');
   expect(response).toBe('error');
 });
